8bbd1ce61cf4d6c0baf6db7c92626b5a460b7635
otniel237 <feunoufossootniel@gmail.com>
2026-09-01 16:15:21 +0200

    finish

--- apps/wallet/vite.config.js
+++ apps/wallet/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    wasm(),
-    topLevelAwait(),
     nodePolyfills({
       include: ['buffer', 'crypto', 'stream', 'util'],
       globals: { Buffer: true }
